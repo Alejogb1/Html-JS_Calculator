60b4eb96670c3f323be70cd7065291a719afcf2a
Magniceh <alejogarb25@gmail.com>
2019-09-04 20:01:23 -0300

    Corrected const key and const action error

--- dist/hello.js
+++ dist/hello.js
@@ -1,49 +1,43 @@
-const calculator = document.querySelector(".calculator");
-const keys = calculator.querySelector(".calculator_keys");
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator_keys')
+const display = document.querySelector('.calculator_display')
 
-keys.addEventListener("click", e => {
-    if (e.target.matches("button")) {
-      // Do something
-      const key = e.target
-      const action = key.dataset.action
-      console.log(key)
-   }})
-
-
-
-   if (!action) {
-    console.log("number key!")
-  }
-if (
-    action === "add",
-    action === "subtract",
-    action === "multiply",
-    action === "divide"
-) {
-    console.log("operator key!")
-}
-if (action === "clear"){
-    console.log("clear key!")
-}
-if (action === "decimal"){
-    console.log("decimal key!")
-}
-if (action === "calculate"){
-    console.log("equal key!")
-}
-//Display
-const display = document.querySelector(".calculator_display")
-
-keys.addEventListener("click", e => {
-    if (e.target.matches("button")) {
+const displayedNum;
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
         const key = e.target
         const action = key.dataset.action
         const keyContent = key.textContent
         const displayedNum = display.textContent
     }
-}
-)
-if (!action) {
+    if (!action) {
+        console.log('number key!')
+    }
+    
+    if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+      ) {
+        console.log('operator key!')
+      }
+ 
+
+if (action === 'decimal') {
+    console.log('decimal key!')
+  }
+  
+  if (action === 'clear') {
+    console.log('clear key!')
+  }
+  
+  if (action === 'calculate') {
+    console.log('equal key!')
+  }
+})
+
+  /*if (!action) {
     if (displayedNum === "0"){
         display.textContent = keyContent
     }
@@ -54,9 +48,9 @@ if (action === "decimal") {
     display.textContent = displayedNum + "."
 }
 if (
-    action === "add",
-    action === "divide",
-    action === "multiply",
+    action === "add" ||
+    action === "divide" ||
+    action === "multiply" ||
     action === "subtract"
 ) {
     key.classList.add("is-depressed")
@@ -76,4 +70,6 @@ if (!action) {
     display.textContent = keyContent
 } else {
     display.textContent = displayedNum + keyContent
-}
+}*/
+
+ 
